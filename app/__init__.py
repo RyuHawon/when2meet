adb82975a46c7aad89cc0b82150c6120c9ea88b2
RyuHawon <hfixjsj49@gmail.com>
2026-03-03 12:27:00 +0900

    when2meet 스펙생성 중간저장

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
 
+from app.apis.v1.meeting_router import edgedb_router as meeting_edgedb_router
+from app.apis.v1.meeting_router import mysql_router as meeting_mysql_router
+
 app = FastAPI()
+
+
+app.include_router(meeting_edgedb_router)
+app.include_router(meeting_mysql_router)
