c2edc8daa3b27d4403fc2f223db411edc514d079
RyuHawon <hfixjsj49@gmail.com>
2026-03-03 13:24:41 +0900

    when2meet 스펙생성 중간저장

--- app/utils/base62.py
+++ app/utils/base62.py
@@ -1,6 +1,7 @@
 from typing import Final, ClassVar
 import string
 
+
 class Base62:
     BASE: Final[ClassVar[str]] = string.ascii_letters + string.digits
     BASE_LEN: Final[ClassVar[int]] = len(BASE)
